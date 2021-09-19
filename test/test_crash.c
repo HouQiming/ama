6a8cf4e5e0265fd74bf8106a3592ce0bd6a99303
Qiming HOU <hqm03ster@gmail.com>
2021-09-19 17:41:47 +0800

    postfix: no longer generates N_AIR for argument-less N_CALL another newline overhaul: now "absorbs" head-of-scope indent into indent_level, auto-detects tab_indent-ness for codegen cleanup: nRaw(foo,nSymbol(";")) now converted to N_SEMICOLON

--- test/test_crash.c
+++ test/test_crash.c
@@ -1,6 +1,10 @@
 //example from "https://en.cppreference.com/w/cpp/language/attributes.html":
 void f() {
-  int y[3];
-  y[[] { return 0; }()] = 1;  // error
-  int i [[cats::meow([[]])]]; // OK
+	for (T * item : qAsConst(stash))
+		list->append(list, item);
+
+	if(m_selectedInterface->actionInterface()->doAction(QAccessibleActionInterface::pressAction())){
+		//do nothing
+	}
+	return
 }
