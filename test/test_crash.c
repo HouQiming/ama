368d9dab14875407f14f785149c24042f56c6c0f
Qiming HOU <hqm03ster@gmail.com>
2021-09-23 21:09:49 +0800

    decl: constructor parsing

--- test/test_crash.c
+++ test/test_crash.c
@@ -1,4 +1,10 @@
 //LICENSE
+AArch64TargetLowering::AArch64TargetLowering(const TargetMachine &TM,
+                                             const AArch64Subtarget &STI)
+    : TargetLowering(TM), Subtarget(&STI) {
+    //should be a function
+}
+
 void f() {
 	for (T * item : qAsConst(stash))
 		list->append(list, item);
@@ -13,6 +19,7 @@ void f() {
 	if(1){
 		for (int i = 0; i < 10000; ++i)
 			QMetaType::fromName("Foo");
+		test(case 3: case 4:a?b?c:d?e:f?g?h:i);
 	}
 	return
 }
