a61c07f1b7f9f7a8e4d523618c9d20ee4c523607
Qiming HOU <hqm03ster@gmail.com>
2021-10-02 09:02:37 +0800

    cmake: param merging works

--- test/cmake/test_cmake.c
+++ test/cmake/test_cmake.c
@@ -5,7 +5,7 @@
 	let nd_cmake=require('cmake').LoadCMakeFile('/home/hqm/tp/ama/src/entry/CMakeLists.txt');
 	//console.log(JSON.stringify(nd_cmake,null,1))
 	//console.log(nd_cmake.toSource())
-	console.log(nd_cmake.CMakeFindTarget('ama'))
+	console.log(nd_cmake.CMakeFindTarget('ama').toSource())
 #endif
 
 int main(){
