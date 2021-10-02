6b6abdfcb67a8abc1d1ee650ff9ee23d4e8a56ad
Qiming HOU <hqm03ster@gmail.com>
2021-10-02 08:53:38 +0800

    decl: various indent / space / comment fixes

--- modules/jcs.js
+++ modules/jcs.js
@@ -2,17 +2,17 @@
 let jcs=module.exports;
 
 jcs.TranslateJCS=function(nd_root){
-	for(let nd of nd_root.FindAll(N_SYMBOL,'!==')){
+	for(let nd of nd_root.FindAll(N_BINOP,'!==')){
 		nd.data='!=';
 	}
-	for(let nd of nd_root.FindAll(N_SYMBOL,'===')){
+	for(let nd of nd_root.FindAll(N_BINOP,'===')){
 		nd.data='==';
 	}
 	nd_root.Save()
-	for(let nd of nd_root.FindAll(N_SYMBOL,'!=')){
+	for(let nd of nd_root.FindAll(N_BINOP,'!=')){
 		nd.data='!==';
 	}
-	for(let nd of nd_root.FindAll(N_SYMBOL,'==')){
+	for(let nd of nd_root.FindAll(N_BINOP,'==')){
 		nd.data='===';
 	}
 	nd_root.NodeofToASTExpression().Save('.js');
--- test/cmake/test_cmake.c
+++ test/cmake/test_cmake.c
@@ -3,8 +3,9 @@
 #if 0
 	@ama
 	let nd_cmake=require('cmake').LoadCMakeFile('/home/hqm/tp/ama/src/entry/CMakeLists.txt');
-	console.log(JSON.stringify(nd_cmake,null,1))
-	console.log(nd_cmake.toSource())
+	//console.log(JSON.stringify(nd_cmake,null,1))
+	//console.log(nd_cmake.toSource())
+	console.log(nd_cmake.CMakeFindTarget('ama'))
 #endif
 
 int main(){
