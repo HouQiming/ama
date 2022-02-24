b2f75f6da09ca78c620be2a5317b9568e581a1cf
Qiming HOU <hqm03ster@gmail.com>
2022-02-24 23:58:28 +0800

    _init: more JS config fix

--- modules/_init.js
+++ modules/_init.js
@@ -563,12 +563,13 @@ __global.GetPipelineFromFilename=function(filename,default_pipeline){
 		//p.splice(p.indexOf('ParsePointedBrackets'),1);
 		p.unshift({
 			keywords_statement: 'return throw break continue',
-			prefix_operators: '++ -- ! ~ + - * && & ... typeof void delete await new void',
+			prefix_operators: '++ -- ! ~ + - * && & ... typeof void delete await new void yield',
 			postfix_operators: '++ --',
 			cv_qualifiers: '',
-			named_operators: 'typeof delete await new in of instanceof as void',
+			named_operators: 'typeof delete await new in of instanceof as void yield',
 			parse_js_regexp: 1,
 			shell_string_quotes:'`',
+			keywords_operator_escape: 'case in of as const',
 		});
 		if(Node.InsertJSSemicolons){
 			p.splice(p.indexOf('DelimitCLikeStatements'),0,'InsertJSSemicolons');
